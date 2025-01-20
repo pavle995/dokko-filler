78a6c8b46f12767347d94559df06779ac44bd282
Petar Stankovic <stankovicpetar23@gmail.com>
2025-01-20 21:47:17 +0100

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import ThemeProvider from '~components/ThemeProvider/ThemeProvider';
-import ErrorBoundary from '~components/ErrorBoundary/ErrorBoundary';
-import GlobalStyle from '../utils/GlobalSyile';
-import Root from '~components/Root/Root';
-import Login from '~components/Authentication/Login';
-import Signup from '~components/Authentication/Signup';
+import React from "react";
+import ThemeProvider from "~components/ThemeProvider/ThemeProvider";
+import ErrorBoundary from "~components/ErrorBoundary/ErrorBoundary";
+import GlobalStyle from "../utils/GlobalSyile";
+import Root from "~components/Root/Root";
 
 
 function App() {
   return (
-    <Login />
+    <ThemeProvider>
+      <ErrorBoundary>
+        <GlobalStyle />
+        <Root />
+      </ErrorBoundary>
+    </ThemeProvider>
   );
-  // return (
-  //   <ThemeProvider>
-  //     <ErrorBoundary>
-  //       <GlobalStyle />
-  //       <Root />
-  //     </ErrorBoundary>
-  //   </ThemeProvider>
-  // );
 }
 
 export default App;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from '~components/Layout/Layout';
-import AboutPage from '~pages/AboutPage/AboutPage';
-import DocumentsList from '~pages/DocumentsList/DocumetsList';
-import PageNotFound from '~pages/PageNotFound/PageNotFound';
-import HomePage from '~pages/HomePage/HomePage';
-import GenerateDocumentPage from '~pages/GenerateDocumentPage/GenerateDocumentPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "~components/Layout/Layout";
+import AboutPage from "~pages/AboutPage/AboutPage";
+import DocumentsList from "~pages/DocumentsList/DocumetsList";
+import PageNotFound from "~pages/PageNotFound/PageNotFound";
+import HomePage from "~pages/HomePage/HomePage";
+import GenerateDocumentPage from "~pages/GenerateDocumentPage/GenerateDocumentPage";
+import Login from "~components/Authentication/Login";
+import Signup from "~components/Authentication/Signup";
 
 function Root() {
   return (
@@ -13,6 +15,8 @@ function Root() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/documents" element={<DocumentsList />} />
           <Route
             path="/documents/create/:name/:id"
