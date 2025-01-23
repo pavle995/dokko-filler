0ab9b3d5140915bcb39fa605f76d5c992c791257
Petar Stankovic <stankovicpetar23@gmail.com>
2025-01-23 17:54:04 +0100

    Style added to login and signup components.

--- src/shared-components/shared-styled-components/FormWrapper/FormWrapper.jsx
+++ src/shared-components/shared-styled-components/FormWrapper/FormWrapper.jsx
@@ -4,9 +4,9 @@ const FormWrapper = styled.div`
   max-width: 400px;
   margin: auto auto;
   padding: 20px;
-  border: 1px solid #ccc;
+  border: 1px solid ${(props) => props.theme.palette.grey[800]};
   border-radius: 8px;
-  background-color: #f9f9f9;
+  background-color: ${(props) => props.theme.palette.background.paper};
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
